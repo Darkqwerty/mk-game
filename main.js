98f2ede91f1fe442ef1ec6a025e7030b36e3712b
Darkqwerty <darkqwerty.2.0@gmail.com>
2021-04-23 00:40:16 +0300

    homework-8

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
-import { $arenas, createPlayer } from './dom.js';
-import { player1, player2 } from './player.js';
-import generateLogs from './log.js';
+import Game from './game.js';
 
-const $player1 = createPlayer(player1);
-const $player2 = createPlayer(player2);
+const game = new Game();
 
-$arenas.appendChild($player1);
-$arenas.appendChild($player2);
-
-generateLogs('start', player1.name, player2.name);
+game.start();
